311a60a89426fe1d423014a1fe35c2cfefd193be
antikyth3r4 <antikyth3r4@gmail.com>
2015-06-18 05:37:02 +0300

    cleaned up, made make file

--- set1/set1.c
+++ set1/set1.c
@@ -1,6 +1,21 @@
 
 #include "set1.h"
 
+const char hex_to_int[] = { [0 ... 255] = -1,
+                            ['0'] = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                            ['A'] = 10, 11, 12, 13, 14, 15,
+                            ['a'] = 10, 11, 12, 13, 14, 15         };
+
+const char int_to_hex[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+const char base64Symbols[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                              'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                              'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                              'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                              'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                              'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                              'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                              '4', '5', '6', '7', '8', '9', '+', '/'};
 
 /**
  * Convert a hex array to a base64 string.
@@ -52,8 +67,8 @@ char * fixed_xor(const char * string1, const char * string2, size_t size){
     int i;
     char * result = malloc(size);
     for(i=0;i<size;i++){
-        uint8_t a = hex_to_int[string1[i]];
-        uint8_t b = hex_to_int[string2[i]];
+        uint8_t a = hex_to_int[(uint8_t)string1[i]];
+        uint8_t b = hex_to_int[(uint8_t)string2[i]];
         result[i] = int_to_hex[a^b];
     }
     return result;
--- set1/set1.h
+++ set1/set1.h
@@ -4,23 +4,11 @@
 
 #include "myHeaders.h"
 
+extern const char hex_to_int[];
+extern const char int_to_hex[];
+extern const char base64Symbols[];
+
 char * hex_to_base64(const char * data, size_t input_size, size_t * output_size);
 char * fixed_xor(const char * string1, const char * string2, size_t size);
-static const char hex_to_int[] = { [0 ... 255] = -1,
-                            ['0'] = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                            ['A'] = 10, 11, 12, 13, 14, 15,
-                            ['a'] = 10, 11, 12, 13, 14, 15         };
-
-static const char int_to_hex[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-
-static const char base64Symbols[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-                              'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                              'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-                              'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-                              'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                              'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                              'w', 'x', 'y', 'z', '0', '1', '2', '3',
-                              '4', '5', '6', '7', '8', '9', '+', '/'};
-
 
 #endif
